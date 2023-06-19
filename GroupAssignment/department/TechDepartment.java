c8937af623511153290fd9addb206e2d896064f1
NeerajLaul <135740179+NeerajLaul@users.noreply.github.com>
2023-06-19 22:46:01 +0530

    Update TechDepartment.java
    Added Missing Full Stops In String.

--- GroupAssignment/department/TechDepartment.java
+++ GroupAssignment/department/TechDepartment.java
@@ -2,19 +2,19 @@ package com.group.department;
 
 public class TechDepartment extends SuperDepartment{
 	public String departmentName() {
-		return ("Tech Department");
+		return ("Tech Department.");
 	}
 	
 	public String getTodaysWork() {
-		return ("Complete coding of module 1");
+		return ("Complete coding of module 1.");
 	}
 	
 	public String getWorkDeadline() {
-		return ("Complete By EOD");
+		return ("Complete By EOD.");
 	}
 	
 	public String getTechStackInformation() {
-		return ("Core JAVA");
+		return ("Core JAVA.");
 	}
 
 }
