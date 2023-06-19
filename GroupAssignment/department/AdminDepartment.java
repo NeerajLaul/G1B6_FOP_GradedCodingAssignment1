0db597d1b95d6daed63add23530d133fa599b163
NeerajLaul <135740179+NeerajLaul@users.noreply.github.com>
2023-06-19 22:43:25 +0530

    Update AdminDepartment.java
    Added Missing Full stops.

--- GroupAssignment/department/AdminDepartment.java
+++ GroupAssignment/department/AdminDepartment.java
@@ -2,15 +2,15 @@ package com.group.department;
 
 public class AdminDepartment extends SuperDepartment{
 	public String departmentName() {
-		return ("Admin Department");
+		return ("Admin Department.");
 	}
 	
 	public String getTodaysWork() {
-		return ("Complete Documents Submission");
+		return ("Complete Documents Submission.");
 	}
 	
 	public String getWorkDeadline() {
-		return ("Complete By EOD");
+		return ("Complete By EOD.");
 	}
 
 }
